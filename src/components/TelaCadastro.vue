95640566dda59339b308b7bb96f5df1e2af082fc
unknown <willianroby@gmail.com>
2020-05-12 20:38:29 -0300

    salvar cadastro feito

--- src/components/TelaCadastro.vue
+++ src/components/TelaCadastro.vue
@@ -19,11 +19,13 @@
        <br>
        <input type="text" placeholder="Bairro" maxlength="25" v-model="bairro" >
        <br>
+       <input type="text" onkeypress="return event.charCode >= 48 && event.charCode <= 57" placeholder="Número" maxlength="5" v-model="numero">
+       <br>
        <input type="text" placeholder="UF" maxlength="2" v-model="uf">
        <br>
        <input type="text" placeholder="Complemento" maxlength="50" v-model="complemento">
        <br>
-       <button id="botãoCadastrar" @click="salvarCadastro()">Cadastrar</button>
+       <button id="botãoCadastrar" @click="salvarCadastro">Cadastrar</button>
        <br>
        <button id="botãoVoltar" @click="voltarMenu">Voltar para o Menu Principal</button> 
   </div>
@@ -44,15 +46,15 @@ export default {
         cidade: '',
         logradouro: '',
         bairro: '',
+        numero: '',
         uf: '',
         complemento: ''
         }
     }, methods:{
         voltarMenu:function(){
             this.$router.push("/")
-        }
-    }, 
-    onlynumber: function(evt){
+        }, 
+        onlynumber: function(evt){
         var theEvent = evt || window.event;
         var key = theEvent.keyCode || theEvent.which;
         key = String.fromCharCode( key );
@@ -61,7 +63,7 @@ export default {
         if( !regex.test(key) ) {
         theEvent.returnValue = false;
         if(theEvent.preventDefault) theEvent.preventDefault();
-         }
+        }
     }, salvarCadastro: function(){
         axios.post("http://localhost:55537/api/Cliente",{
             nome: this.nome,
@@ -73,27 +75,31 @@ export default {
             cidade: this.cidade,
             logradouro: this.logradouro,
             bairro: this.bairro,
+            numero: this.numero,
             uf: this.uf,
             complemento: this.complemento
         }).then(resp =>{
             console.log(resp.data)
             this.cliente.push({
-                nome: this.nome,
-                telefone: this.telefone,
-                email: this.email,
-                senha: this.senha,
-                cpf: this.cpf,
-                cep: this.cep,
-                cidade: this.cidade,
-                logradouro: this.logradouro,
-                bairro: this.bairro,
-                uf: this.uf,
-                complemento: this.complemento
+            nome: this.nome,
+            telefone: this.telefone,
+            email: this.email,
+            senha: this.senha,
+            cpf: this.cpf,
+            cep: this.cep,
+            cidade: this.cidade,
+            logradouro: this.logradouro,
+            bairro: this.bairro,
+            numero: this.numero,
+            uf: this.uf,
+            complemento: this.complemento
+                })
             })
-        })
+        }
     },mounted(){
         axios.get("http://localhost:55537/api/Cliente").then(cliente => this.clientes = cliente.data)
     }
+    
 }
 </script>
 
