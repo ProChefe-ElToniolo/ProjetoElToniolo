9205e7978aa0cedbbbe292a35c99aeb71809d87e
nathan-ngp <nathan_piran@estudante.sc.senai.br>
2020-05-12 16:54:22 -0300

    Tela Categoria terminada por Nathan

--- src/components/TelaCadastroCategoria.vue
+++ src/components/TelaCadastroCategoria.vue
@@ -2,26 +2,108 @@
   <div>
       <h1>Bem Vindo a Tela Cadastro Categoria</h1>
       <button @click="IrParaTelaMenuAdmin">IrParaTelaMenuAdmin</button>
-      <input type="text" placeholder="Digite o nome da Categoria" id="txtCategoria">
+      <input type="text" placeholder="Digite o nome da Categoria" id="txtCategoria" v-model="nome">
+      <span v-if="verificado">O seu arrombadinho de merda, digita algo na txt PORRAAAA</span>
       <input type="file">  
-      <img src="" alt="" id="output">
+        <ul>
+            <li v-for="cat in categorias" :key="cat.id">
+                {{cat.nome}}
+            </li>
+        </ul>
+        
+        <select v-model="catId">
+            <option value="0" selected disabled>Selecione o ID da categoria</option>
+            <option v-for="cat in categorias" :key="cat.id" :value="cat.id">
+                {{cat.id}} -- {{cat.nome}}
+            </option>
+        </select>
+
+        <button @click="SalvarCategoria">Salvar Categoria</button>
+        <button @click="ExcluirCategoria">Excluir Categoria</button>
+        <button @click="AlterarCategoria">Alterar Categoria</button>
+        
+        <table border="5" style="cursor:pointer"> 
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="cat in categorias" :key="cat.id" @click="SelecionarCategoria(cat.id)">
+                    <td>{{cat.id}}</td>
+                    <td>{{cat.nome}}</td>
+                </tr>
+            </tbody>
+        </table>
   </div>
 </template>
 
 <script>
+const axios = require('axios')
 
 export default {
 data:function(){
-    return{
-
+    return{      
+            nome:'',
+            catId:0,
+            categorias:[],
+            categoriasAlterar:[],
+            verificado: false 
     }
 }, methods:{
     IrParaTelaMenuAdmin:function(){
         this.$router.push("/ViewTelaMenuAdmin")
+    }, 
+    
+    SalvarCategoria:function(){
+        var txtCategoria = document.getElementById("txtCategoria")
+        console.log(txtCategoria)
+        if(txtCategoria.value != ''){
+        this.verificado = false
+        axios.post("http://localhost:55537/api/Categoria", {
+            nome: this.nome
+        }).then(resp => {
+            console.log(resp.data)
+            this.categorias.push({
+                nome: this.nome
+            })
+        })
+        } else{
+            this.verificado = true
+        }
+        window.location.reload()
+    },
+    
+    SelecionarCategoria:function(id){
+        this.categorias.filter(c =>{
+            if(c.id == id){
+                this.categoriasAlterar = c
+            }
+        })
+        this.nome = this.categoriasAlterar.nome
+    },  
+
+    AlterarCategoria:function(){
+        axios.put("http://localhost:55537/api/Categoria/" + this.categoriasAlterar.id, {
+            nome: this.nome
+        }).then(resp => console.log(resp.data))
+        window.location.reload()
+    },
+    
+    ExcluirCategoria:function(){
+        console.log(this.catId)
+        axios.delete("http://localhost:55537/api/Categoria/" + this.catId).then(resp => console.log(resp.data))
+        window.location.reload()
     }
 }, 
+
+    mounted(){
+    axios.get("http://localhost:55537/api/Categoria").then(categoria => this.categorias = categoria.data)
+}
 }
 </script>
 
 <style>
+
 </style>
